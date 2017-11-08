8ae9a0aab5d09529d6f39b2563c5bd11b7287c35
Smith <simonsm@student.chalmers.se>
2017-11-08 20:36:47 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,12 @@ fn main() {
 	let c1 = 1;
 	let c2 = 0.1;
 	let alpha = 1;
-	let delta_t = 1;
-	let r = 2; // radius
-	let T = 5;
+	let t_delta = 1;
 	let w = 1.5; // Inertia weight
 	let beta = 0.99; // to reduce w
 	let w_lower_bound = 0.4;
 	let v_max = 0.15;
-	let dimensions = 2;
+	//let dimensions = 2;
 	let swarm_best_position = (0.0, 0.0); // x^sb
 	let threshold = 0.00001;
 
@@ -29,20 +27,23 @@ fn main() {
 	println!("and its value: {:?}", evalutate_particle(p));
 }
 
-type vector = (f32, f32);
+type Vector = (f32, f32);
 
 #[derive(Debug)]
 struct Particle {
-	pos: vector, // x_ij
-	vel: vector, // v_ij
-	best_pos: vector, // x_i^bp
+	pos: Vector, // x_ij
+	vel: Vector, // v_ij
+	best_pos: Vector, // x_i^bp
 }
 
 // Initialize positions and velocities of the particles pi
 fn initialize_particles(n_particles: usize) -> Vec<Particle>{
 	let mut particles: Vec<Particle> = Vec::with_capacity(n_particles);
     let mut rng = rand::thread_rng();
-    let xmax = 5.0;
+    const X_MAX: f32 = 5.0;
+    const X_MIN: f32 = -5.0;
+    const ALPHA: f32 = 1.0;
+    const T_DELTA: f32 = 1.0;
 
    	for _ in 0..n_particles{
 	    let r1: f32 = rng.gen();
@@ -50,13 +51,12 @@ fn initialize_particles(n_particles: usize) -> Vec<Particle>{
 	    let r3: f32 = rng.gen();
 	    let r4: f32 = rng.gen();
 
-		// xij = xmin +r(xmax - xmin)
-	    // vij = α/deltaT(−(xmax−xmin)/2 + r(xmax−xmin))
-	    // 	   or  = α(xmin + r(xmax−xmin))/deltaT, xmin=-xmax
-	    let x1 = r1 * 2.0 * xmax - xmax;
-	    let x2 = r2 * 2.0 * xmax - xmax;
-	    let v1 = r3 * 2.0 * xmax - xmax; // alpha = deltaT = 1, xmax=-xmin
-	    let v2 = r4 * 2.0 * xmax - xmax; // alpha = deltaT = 1, xmax=-xmin
+		// xij = X_MIN +r(X_MAX - X_MIN)
+	    let x1 = X_MIN + r1*(X_MAX - X_MIN);
+	    let x2 = X_MIN + r2*(X_MAX - X_MIN);
+	    // vij = α/delta_t(-(X_MAX-X_MIN)/2 + r(X_MAX-X_MIN))
+	    let v1 = ALPHA/T_DELTA*(-(X_MAX-X_MIN)/2.0 + r3*(X_MAX-X_MIN));
+	    let v2 = ALPHA/T_DELTA*(-(X_MAX-X_MIN)/2.0 + r4*(X_MAX-X_MIN));
 	    
 	    particles.push(
 	    	Particle {
